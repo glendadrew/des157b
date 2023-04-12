782e43143e8c4229243817aaff6419833ffc96aa
glendadrew <gadrew@ucdavis.edu>
2023-04-11 20:07:46 -0700

    clean js

--- jaime-grid/script.js
+++ jaime-grid/script.js
@@ -2,21 +2,24 @@
     'use strict';
 
     const button = document.querySelector('button');
+    const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
+    const sections = document.querySelectorAll('section');
+    let mode='dark';
+
     const colors = ['#fcbe02', '#f76663', '#d9d3d3', '#2f8fd9', '#157575', '#f29e31', '#f4b8c5', '#9edad7', '#ed6f78', '#08938a', '#df5087', '#4dbc92', '#ef6c4a', '#e73f51', '#d7e022', '#f55435', '#ecd257', '#4fa3a4', '#fdae39', '#fa5619', '#f1962d', '#dce1dc', '#e7cd39', '#f5b95c', '#51c8d2', '#f37b6c', '#ffaacf', '#c5dffa', '#bfedaa', '#fffbfa'];
     const grays = ['#e0dad1', '#2e3535', '#767972', '#545c58', '#a9a69a', '#5c5c5b', '#95938c', '#949084', '#5c6454', '#f0ebe4'];
-    let bannerSwitch = false;
-    const body = document.querySelector('body');
-    const sections = document.querySelectorAll('section');
 
     button.addEventListener('click', function () {
-        if (!bannerSwitch) {
+        if (mode === 'dark') {
             body.className = 'switch';
             // banner.className = 'switch';
             button.className = 'switch';
             for (const section of sections) {
                 section.className = 'switch';
-            } makeGrayDivs();
+            } 
+            mode = 'light';
+            makeGrayDivs();
         } else {
             body.removeAttribute('class');
             // banner.removeAttribute('class');
@@ -24,9 +27,9 @@
             for (const section of sections) {
                 section.removeAttribute('class');
             }
+            mode = 'dark';
             makeColorDivs();
         }
-        bannerSwitch = !bannerSwitch;
     })
 
     // make initial call to function
